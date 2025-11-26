51a9924d5cc0585988558dd87ff6cac49eecb816
kurumakazuto <153343673+kurumakazuto@users.noreply.github.com>
2025-11-26 12:30:15 +0900

    chore: prettierテスト5

--- components/auth/LoginForm.vue
+++ components/auth/LoginForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const userInfo = ref<LoginInfo>({ userId: "", password: "" });
+const userInfo = ref<LoginInfo>({ userId: '', password: '' });
 
 watch(
   () => userInfo.value.userId,
@@ -7,7 +7,7 @@ watch(
     if (newVal && newVal !== newVal.trim()) {
       userInfo.value.userId = newVal.trim();
     }
-  },
+  }
 );
 
 // バリデーション
@@ -33,23 +33,21 @@ function hasSpace(password: string) {
 // 即時チェック
 const idError = computed(() => {
   const id = userInfo.value.userId;
-  if (!id) return "";
-  if (isInvalidFormat(id)) return "メールアドレスの形式にしてください";
-  if (isUppercaseIncluded(id))
-    return "メールアドレスは小文字で入力してください";
-  if (isTooLong(id)) return "100文字以内で入力してください";
-  return "";
+  if (!id) return '';
+  if (isInvalidFormat(id)) return 'メールアドレスの形式にしてください';
+  if (isUppercaseIncluded(id)) return 'メールアドレスは小文字で入力してください';
+  if (isTooLong(id)) return '100文字以内で入力してください';
+  return '';
 });
 
 const passwordError = computed(() => {
-  const password = userInfo.value.password ?? "";
-  if (!password) return "";
-  if (isTooShort(password)) return "8文字以上で入力してください";
-  if (isTooLong(password)) return "100文字以内で入力してください";
-  if (isMissingLetterOrNumber(password))
-    return "英字と数字を両方含めてください";
-  if (hasSpace(password)) return "スペースは使用できません";
-  return "";
+  const password = userInfo.value.password ?? '';
+  if (!password) return '';
+  if (isTooShort(password)) return '8文字以上で入力してください';
+  if (isTooLong(password)) return '100文字以内で入力してください';
+  if (isMissingLetterOrNumber(password)) return '英字と数字を両方含めてください';
+  if (hasSpace(password)) return 'スペースは使用できません';
+  return '';
 });
 
 // 送信時チェック
@@ -68,9 +66,7 @@ function validate() {
       v-model="userInfo.userId"
       :class="[idError ? 'border-red-500' : '']"
     />
-    <span class="w-full text-red-500" v-if="idError">{{
-      `！ ${idError}`
-    }}</span>
+    <span class="w-full text-red-500" v-if="idError">{{ `！ ${idError}` }}</span>
     <label class="block">パスワード</label>
     <input
       type="text"
@@ -78,15 +74,10 @@ function validate() {
       class="w-full border border-gray-300 bg-white text-gray-900 px-3 py-3 focus:outline-none rounded-xl shadow-md"
       v-model="userInfo.password"
     />
-    <span class="w-full text-red-500" v-if="passwordError">{{
-      `！ ${passwordError}`
-    }}</span>
+    <span class="w-full text-red-500" v-if="passwordError">{{ `！ ${passwordError}` }}</span>
   </div>
   <div class="text-center">
-    <UiButton
-      label="登録"
-      :disabled="Boolean(idError) || Boolean(passwordError)"
-    />
+    <UiButton label="登録" :disabled="Boolean(idError) || Boolean(passwordError)" />
   </div>
 </template>
 
